c9560b7386f27b46fb38d480f2e44a8897fd630b
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-12-03 16:27:24 +0100

    Ejercicio base

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+let mano = 0
+input.onGesture(Gesture.Shake, function () {
+    mano = randint(1, 3)
+    if (mano == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (mano == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
