c013c2a060afda063cefa8ec932cadb657c358d9
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-12-08 15:10:00 +0100

    Mejora: muestra de resultado sincronizada

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-def on_received_number(receivedNumber):
-    global mano_oponente, confirmacion
-    mano_oponente = receivedNumber
-    confirmacion = True
-    radio.send_value("recibido", 1)
-    if listo_mostrar and confirmacion:
-        resultado()
-radio.on_received_number(on_received_number)
-
-def resultado():
-    global listo_mostrar
-    if mano_oponente == mano:
-        basic.show_icon(IconNames.ASLEEP)
-        music._play_default_background(music.built_in_playable_melody(Melodies.POWER_DOWN),
-            music.PlaybackMode.IN_BACKGROUND)
-    elif mano == 1 and mano_oponente == 3 or (mano == 3 and mano_oponente == 2 or mano == 2 and mano_oponente == 1):
-        basic.show_icon(IconNames.HAPPY)
-        music._play_default_background(music.built_in_playable_melody(Melodies.POWER_UP),
-            music.PlaybackMode.IN_BACKGROUND)
-    elif False:
-        basic.show_icon(IconNames.SAD)
-        music._play_default_background(music.built_in_playable_melody(Melodies.FUNERAL),
-            music.PlaybackMode.IN_BACKGROUND)
+def mostrar():
+    if juez == 1:
+        if resultado2 == 0:
+            basic.show_icon(IconNames.ASLEEP)
+        elif resultado2 == 1:
+            basic.show_icon(IconNames.HAPPY)
+        else:
+            basic.show_icon(IconNames.SAD)
     else:
-        pass
-    listo_mostrar = False
-    listo_mostrar = False
+        if resultado_recibido == 0:
+            basic.show_icon(IconNames.ASLEEP)
+        elif resultado_recibido == 2:
+            basic.show_icon(IconNames.HAPPY)
+        else:
+            basic.show_icon(IconNames.SAD)
+def resultado():
+    global resultado2
+    basic.show_number(3)
+    basic.pause(500)
+    basic.show_number(2)
+    basic.pause(500)
+    basic.show_number(1)
+    basic.pause(500)
+    if mi_mano == mano_oponente:
+        resultado2 = 0
+    elif mi_mano == 1 and mano_oponente == 3 or (mi_mano == 3 and mano_oponente == 2 or mi_mano == 2 and mano_oponente == 1):
+        resultado2 = 1
+    else:
+        resultado2 = 2
 
 def on_gesture_shake():
-    global mano
-    mano = randint(1, 3)
-    if mano == 1:
+    global mi_mano
+    mi_mano = randint(1, 3)
+    if mi_mano == 1:
         basic.show_leds("""
             . . . . .
             . # # # .
@@ -37,7 +39,7 @@ def on_gesture_shake():
             . # # # .
             . . . . .
             """)
-    elif mano == 2:
+    elif mi_mano == 2:
         basic.show_leds("""
             # # # # #
             # # # # #
@@ -45,7 +47,7 @@ def on_gesture_shake():
             # # # # #
             # # # # #
             """)
-    elif mano == 3:
+    elif mi_mano == 3:
         basic.show_leds("""
             # . . . #
             . # . # .
@@ -61,22 +63,33 @@ def on_gesture_shake():
             . . . . .
             . . . . .
             """)
-    radio.send_number(mano)
-    radio.send_value("listo", 1)
+    radio.send_value(mano, mi_mano)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_received_value(name, value):
-    global listo_mostrar
-    if name == "listo" or name == "recibido":
-        listo_mostrar = True
-        if confirmacion:
-            resultado()
+    global mano_oponente, esperando_oponente, resultado_recibido
+    if name == mano:
+        if juez == 1:
+            mano_elegida = 0
+            mano_oponente = value
+            if mano_elegida:
+                resultado()
+                radio.send_number(resultado2)
+            else:
+                esperando_oponente = True
+        else:
+            resultado_recibido = value
+            mostrar()
 radio.on_received_value(on_received_value)
 
-mano = 0
-confirmacion = False
+esperando_oponente = False
+mano = ""
 mano_oponente = 0
-listo_mostrar = False
+mi_mano = 0
+resultado_recibido = 0
+resultado2 = 0
+juez = 0
 radio.set_group(1)
+juez = randint(0, 1)
 listo_mostrar = False
 listo_mostrar = False
--- main.ts
+++ main.ts
@@ -1,15 +1,43 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    mano_oponente = receivedNumber
-    confirmacion = true
-    radio.sendValue("recibido", 1)
-    if (listo_mostrar && confirmacion) {
-        basic.pause(500)
-        radio.sendString("mostrar")
+function mostrar () {
+    if (juez == 1) {
+        if (resultado_final == 0) {
+            basic.showIcon(IconNames.Asleep)
+        } else if (resultado_final == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+    } else {
+        if (resultado_final == 0) {
+            basic.showIcon(IconNames.Asleep)
+        } else if (resultado_final == 2) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
     }
-})
+    ya_elegida = false
+}
+// Solo la placa juez
+function resultado2 () {
+    basic.showNumber(3)
+    basic.pause(300)
+    basic.showNumber(2)
+    basic.pause(300)
+    basic.showNumber(1)
+    basic.pause(300)
+    if (mi_mano == mano_oponente) {
+        resultado_final = 0
+    } else if (mi_mano == 1 && mano_oponente == 3 || (mi_mano == 3 && mano_oponente == 2 || mi_mano == 2 && mano_oponente == 1)) {
+        resultado_final = 1
+    } else {
+        resultado_final = 2
+    }
+}
 input.onGesture(Gesture.Shake, function () {
-    mano = randint(1, 3)
-    if (mano == 1) {
+    mi_mano = randint(1, 3)
+    ya_elegida = true
+    if (mi_mano == 1) {
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -17,7 +45,7 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
-    } else if (mano == 2) {
+    } else if (mi_mano == 2) {
         basic.showLeds(`
             # # # # #
             # # # # #
@@ -25,7 +53,7 @@ input.onGesture(Gesture.Shake, function () {
             # # # # #
             # # # # #
             `)
-    } else if (mano == 3) {
+    } else if (mi_mano == 3) {
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -42,42 +70,27 @@ input.onGesture(Gesture.Shake, function () {
             . . . . .
             `)
     }
-    radio.sendNumber(mano)
-    radio.sendValue("listo", 1)
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showNumber(3)
-    basic.pause(500)
-    basic.showNumber(2)
-    basic.pause(500)
-    basic.showNumber(1)
-    basic.pause(500)
-    if (mano_oponente == mano) {
-        basic.showIcon(IconNames.Asleep)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
-    } else if (mano == 1 && mano_oponente == 3 || (mano == 3 && mano_oponente == 2 || mano == 2 && mano_oponente == 1)) {
-        basic.showIcon(IconNames.Happy)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
-    } else {
-        basic.showIcon(IconNames.Sad)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
-    }
-    listo_mostrar = false
-    listo_mostrar = false
+    radio.sendValue("mano", mi_mano)
 })
 radio.onReceivedValue(function (name, value) {
-    if (name == "listo" || name == "recibido") {
-        listo_mostrar = true
-        if (listo_mostrar && confirmacion) {
-            basic.pause(500)
-            radio.sendString("mostrar")
+    if (name == "mano") {
+        mano_oponente = value
+        if (juez == 1) {
+            if (ya_elegida) {
+                resultado2()
+                radio.sendValue("resultado", resultado_final)
+                mostrar()
+            }
         }
+    } else if (name == "resultado") {
+        resultado_final = value
+        mostrar()
     }
 })
-let mano = 0
-let confirmacion = false
 let mano_oponente = 0
-let listo_mostrar = false
+let mi_mano = 0
+let ya_elegida = false
+let resultado_final = 0
+let juez = 0
 radio.setGroup(1)
-listo_mostrar = false
-listo_mostrar = false
+juez = randint(0, 1)
